24e72422886d90fd6d79f42ceff5bd2ea9722d1f
DHANA5982 <sekar.dhana5982@gmail.com>
2025-08-03 18:57:17 +0100

    UPDATE pipeline files

--- src/model_cluster.py
+++ src/model_cluster.py
@@ -1,27 +1,24 @@
 import pandas as pd
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+import joblib
 
 def run_customer_segmentation(X,_):
-    # Standardizing predictors
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
+    # Pipeline for scaling and cluster modeling
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('kmeans', KMeans(n_clusters=4, random_state=42))
+    ])
+
+    X['Cluster'] = pipeline.fit_predict(X)
+    joblib.dump(pipeline, 'models/segment_model.pkl')
     
-    features = ['Age', 'Gender', 'Tenure', 'Usage_Frequency', 'Support_Calls', 'Last_Interaction',
-                    'Payment_Delay', 'Subscription_Type', 'Total_Spend', 'Contract_Length']
-    
-    # Return preprocessed + a DataFrame version for clustering
-    df_scaled = pd.DataFrame(X_scaled, columns=features)
-
-
-    kmeans = KMeans(n_clusters=4, random_state=42)
-    df_scaled['Segment'] = kmeans.fit_predict(df_scaled)
-
-    # Save segment chart
-    df_scaled['Segment'].value_counts().plot(kind='bar', title='Segment Distribution')
+    print(X['Cluster'].value_counts().plot(kind='bar', title='Segment Distribution'))
     plt.savefig('output/charts/segment_distribution.png')
 
     # Save cluster profiles
-    segment_profiles = df_scaled.groupby('Segment').mean()
+    segment_profiles = X.groupby('Cluster').mean()
     segment_profiles.to_csv('data/cluster/segment_profiles.csv')
+    print(X['Cluster'].value_counts())
